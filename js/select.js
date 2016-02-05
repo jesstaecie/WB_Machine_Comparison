b907a43b5056a16c3f06ef7f094f1a9541232933
jesstaecie <jesswongsg@gmail.com>
2016-02-05 16:34:29 +0800

    tried changing colour of add icon

--- js/select.js
+++ js/select.js
@@ -1,10 +1,26 @@
+// $(function changeImage() {
+// 	var image = document.getElementByID('eagleaeroadd');
+// 	if (image.src = ("eagleaeroadd")) {
+// 		image.src = "img/redadd.png";
+// 	} 
+// 	else {
+// 		image.src = "img/whiteadd.png";
+// 	}
+
+// });
+
+
+
 $ (function () {
 		$(".compare-product").hide();
-	
 
-
-		$("#eagleaero").click(function() {
+		$("#eagleaeroadd" && "#ihawkaeroadd").click(function() {
+			$(".machine-greybox").attr('src', "../img/redadd.png")
+		
+			$(".compare-product").show();
 
 		})
 
 });
+
+
