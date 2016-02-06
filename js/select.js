1ac637505daec832e77747d51e2323bf0f89ce7f
Jia Rong Lim <jr.loves.maths@gmail.com>
2016-02-06 21:49:32 +0800

    Set query string on compare products
    - Able to set querystring on select.html
    - Able to extract querystring from compare.html

--- js/select.js
+++ js/select.js
@@ -1,7 +1,8 @@
 $ (function () {
 		$(".compare-product").hide();
-		// when user selects 2 buttons,
-		// store machine-id and show compare product button
+
+		// When user selects 2 buttons,
+		// tore machine-id and show compare product button
 
 		// 1. show that you have clicked, remove click when you unclick
 		// 2. click machine-id -> get id
@@ -9,7 +10,7 @@ $ (function () {
 		// 4. after click 2, show compare-product
 
 		var firstMachineItemId, secondMachineItemId;
-		$(".machine-item-select").click(function() {
+		$(".select-machine-item-select").click(function() {
 			var machineId = $(this).data("machine-id");
 
 			if (machineId == firstMachineItemId || machineId == secondMachineItemId) { // remove
@@ -32,21 +33,30 @@ $ (function () {
 			}
 
 			if (firstMachineItemId != undefined && secondMachineItemId != undefined) { // full
-				$(".machine-item-select")
-				.not(".machine-item-select[data-machine-id="+firstMachineItemId+"]")
-				.not(".machine-item-select[data-machine-id="+secondMachineItemId+"]")
+				$(".select-machine-item-select")
+				.not(".select-machine-item-select[data-machine-id="+firstMachineItemId+"]")
+				.not(".select-machine-item-select[data-machine-id="+secondMachineItemId+"]")
 				.attr("src", "img/fadedadd.png");
 
 				$(".compare-product").show();
 			} else { // not full
-				$(".machine-item-select")
-				.not(".machine-item-select[data-machine-id="+firstMachineItemId+"]")
-				.not(".machine-item-select[data-machine-id="+secondMachineItemId+"]")
+				$(".select-machine-item-select")
+				.not(".select-machine-item-select[data-machine-id="+firstMachineItemId+"]")
+				.not(".select-machine-item-select[data-machine-id="+secondMachineItemId+"]")
 				.attr("src", "img/whiteadd.png");
 
 				$(".compare-product").hide();
 			}
 
+		});
+
+
+		// When user clicks compare product,
+		// bring user to url with querystring id equals to 2 product id
+
+		$(".compare-product").click(function() {
+			var url = "compare.html" + "?id=" + firstMachineItemId + "," + secondMachineItemId;
+			window.location = url;
 		});
 
 });
